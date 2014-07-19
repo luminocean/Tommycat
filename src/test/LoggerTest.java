c225154b409608518243304dfa08a86fcf8a6925
luminocean <282896922@qq.com>
2014-07-19 16:33:13 +0800

    强化Logger功能

---
+++ src/test/LoggerTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import util.Logger;
+
+public class LoggerTest {
+
+	public static void main(String[] args) {
+		Logger.debug("sasa");
+	}
+
+}
--- src/util/Logger.java
+++ src/util/Logger.java
@@ -2,6 +2,24 @@ package util;
 
 public class Logger {
 	public static void debug(String msg){
-		System.out.println(msg);
+		String callingInfo = getCallingInfo();
+		System.out.println("[debug] "+msg+" "+callingInfo);
+	}
+
+	/**
+	 * 获取该方法的调用方信息
+	 * @return
+	 */
+	private static String getCallingInfo() {
+		StackTraceElement[] stackElements = Thread.currentThread().getStackTrace();
+		
+		if( stackElements.length > 2 ){
+			String callingMethod = stackElements[3].getMethodName();
+			String callingClass = stackElements[3].getClassName();
+			
+			return "("+callingClass+":"+callingMethod+")";
+		}
+
+		return "";
 	}
 }
